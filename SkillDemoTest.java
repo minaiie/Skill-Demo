fab82bd39b81178e1489e9183e47025a319c8388
minaiie <92061511+minaiie@users.noreply.github.com>
2022-02-04 01:36:57 -0800

    failed test

---
+++ SkillDemoTest.java
@@ -0,0 +1,11 @@
+import java.beans.Transient;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void addition() {
+        assertEquals(2, SkillDemo.add(1,2));
+    }
+}
