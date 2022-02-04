3e4aefc867102ecb6211c843e72a4e5ac98bb587
minaiie <92061511+minaiie@users.noreply.github.com>
2022-02-04 01:41:52 -0800

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -6,6 +6,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void addition() {
-        assertEquals(2, SkillDemo.add(1,2));
+        assertEquals(3, SkillDemo.add(1,2));
     }
 }
